6715a776a7a7d11ce2ef651674fdc98a4547e3e0
Tim Riemer <tim.riemer@gmx.de>
2022-12-03 11:40:05 +0100

    refactored day2 to use a Stream and a cleaner strategy for calculating the score

--- day2/src/main/java/mobi/riemer/Puzzle.java
+++ day2/src/main/java/mobi/riemer/Puzzle.java
@@ -4,98 +4,76 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Objects;
-import java.util.stream.Collectors;
 
 public class Puzzle {
-    public static void main(String[] args) {
-        List<String> allLines = new ArrayList<>();
 
-        try (InputStream inputStream = Puzzle.class.getResourceAsStream("/input.txt");
-             BufferedReader reader = new BufferedReader(new InputStreamReader(Objects.requireNonNull(inputStream)))) {
-            allLines = reader.lines()
-                    .collect(Collectors.toList());
+    private static int firstScore = 0;
+    private static int secondScore = 0;
+
+    public static void main(String[] args) {
+        try (InputStream inputStream = Puzzle.class.getResourceAsStream("/input.txt"); BufferedReader reader = new BufferedReader(new InputStreamReader(Objects.requireNonNull(inputStream)))) {
+            reader.lines().forEach(Puzzle::calculateScore);
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-        int scoreFirst = 0;
-        int scoreSecond = 0;
-        for (String line : allLines) {
-            scoreFirst += calculateScoreFirstStrategy(line);
-            scoreSecond += calculateScoreSecond(line);
-        }
-
-        System.out.println("Score by first strategy: " + scoreFirst);
-        System.out.println("Score by second strategy: " + scoreSecond);
+        System.out.println("Score by first strategy: " + firstScore);
+        System.out.println("Score by second strategy: " + secondScore);
     }
 
-    private static int calculateScoreSecond(String line) {
-        int score = 0;
-        List<String> shapes = Arrays.stream(line.split(" ")).toList();
+    private static void calculateScore(String l) {
+        String opponentShape = l.split(" ")[0];
+        String ownShape = l.split(" ")[1];
 
-        String opponentShape = shapes.get(0);
-        String ownShape = shapes.get(1);
-
-        switch (ownShape) {
-            case "X" -> {  // need to loose
-                switch (opponentShape) {
-                    case "A" -> score += 3; // scissor
-                    case "B" -> score += 1; // rock
-                    case "C" -> score += 2; // paper
+        switch (opponentShape) {
+            case "A" -> {
+                switch (ownShape) {
+                    case "X" -> {
+                        firstScore += 4;
+                        secondScore += 3;
+                    }
+                    case "Y" -> {
+                        firstScore += 8;
+                        secondScore += 4;
+                    }
+                    case "Z" -> {
+                        firstScore += 3;
+                        secondScore += 8;
+                    }
                 }
             }
-            case "Y" -> {  // end in a draw
-                score = 3;
-                switch (opponentShape) {
-                    case "C" -> score += 3; // scissor
-                    case "A" -> score += 1; // rock
-                    case "B" -> score += 2; // paper
+            case "B" -> {
+                switch (ownShape) {
+                    case "X" -> {
+                        firstScore += 1;
+                        secondScore += 1;
+                    }
+                    case "Y" -> {
+                        firstScore += 5;
+                        secondScore += 5;
+                    }
+                    case "Z" -> {
+                        firstScore += 9;
+                        secondScore += 9;
+                    }
                 }
             }
-            case "Z" -> {  //need to win
-                score = 6;
-                switch (opponentShape) {
-                    case "A" -> score += 2; // paper
-                    case "B" -> score += 3; // scissor
-                    case "C" -> score += 1; // rock
+            case "C" -> {
+                switch (ownShape) {
+                    case "X" -> {
+                        firstScore += 7;
+                        secondScore += 2;
+                    }
+                    case "Y" -> {
+                        firstScore += 2;
+                        secondScore += 6;
+                    }
+                    case "Z" -> {
+                        firstScore += 6;
+                        secondScore += 7;
+                    }
                 }
             }
         }
-
-        return score;
-    }
-
-    private static int calculateScoreFirstStrategy(String line) {
-        int score = 0;
-        List<String> shapes = Arrays.stream(line.split(" ")).toList();
-
-        String opponentShape = shapes.get(0);
-        String ownShape = shapes.get(1);
-
-        switch (ownShape) {
-            case "X" -> score = 1;
-            case "Y" -> score = 2;
-            case "Z" -> score = 3;
-        }
-
-        if ((opponentShape.equals("C") && ownShape.equals("Z"))
-                || (opponentShape.equals("B") && ownShape.equals("Y"))
-                || (opponentShape.equals("A") && ownShape.equals("X"))) {
-            // draw
-            score += 3;
-        } else if (
-                (opponentShape.equals("C") && ownShape.equals("X"))
-                        || (opponentShape.equals("B") && ownShape.equals("Z"))
-                        || (opponentShape.equals("A") && ownShape.equals("Y"))
-        ) {
-            // win
-            score += 6;
-        }
-
-        return score;
     }
 }
